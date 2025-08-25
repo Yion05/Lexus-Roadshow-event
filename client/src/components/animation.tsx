4cdffe115a856e8b713c228291f8e97f960e9b9b
Yion05 <khoojunyong@gmail.com>
2025-08-26 01:14:32 +0800

    Update animation.tsx

--- client/src/components/animation.tsx
+++ client/src/components/animation.tsx
@@ -5,7 +5,7 @@ const Animation: React.FC = () => {
     <div className="fixed inset-0 z-10 flex flex-col items-center justify-center bg-black animate-pulse scale-120">
       <div> 
         <img 
-          src="https://drive.google.com/file/d/1ucFzlzt5PEBgYwNT0fYi2dcQac0j3dZo/view" 
+          src="https://drive.google.com/file/d/1ucFzlzt5PEBgYwNT0fYi2dcQac0j3dZo/view?usp=sharing" 
           alt="Lexus Logo" 
           className="w-[300px] h-[300px] object-contain"
         />
@@ -16,6 +16,7 @@ const Animation: React.FC = () => {
 
 
 export default Animation;
+
 
 
 
