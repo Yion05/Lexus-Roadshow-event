f8d6cef7bd866f0f435ac712c00cd89ae4f98baa
Yion05 <khoojunyong@gmail.com>
2025-08-25 17:36:49 +0800

    Delete server directory

--- server/src/test/api.test.sql.ts
+++
@@ -1,6 +0,0 @@
-import { DB_CONFIG } from "../service/database";
-
-await new DB_CONFIG().getJSON();
-await new DB_CONFIG().getCSV();
-await new DB_CONFIG().readData();
-await new DB_CONFIG().getPaginatedData(1, 10);
