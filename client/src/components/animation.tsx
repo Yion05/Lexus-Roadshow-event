413f35cf339e521c103593a1b7b71339fab5ca16
Jiaqi0929 <gigilim0929@gmail.com>
2025-08-25 23:25:08 +0800

    Update animation.tsx

--- client/src/components/animation.tsx
+++ client/src/components/animation.tsx
@@ -5,7 +5,7 @@ const Animation: React.FC = () => {
     <div className="fixed inset-0 z-10 flex flex-col items-center justify-center bg-black animate-pulse scale-120">
       <div> 
         <img 
-          src="https://Lexus-Roadshow-event.vercel.app/Lexus_Logo" 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpsVPst5XTXecPPBr3t0gd2a8-DiKDVChY0g&s" 
           alt="Lexus Logo" 
           className="w-[300px] h-[300px] object-contain"
         />
@@ -16,6 +16,7 @@ const Animation: React.FC = () => {
 
 
 export default Animation;
+
 
 
 
